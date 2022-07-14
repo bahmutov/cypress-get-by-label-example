49e722f55d6b80490ce7f69b91f42e9f73ffa0ae
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-07-14 10:22:11 -0400

    ignore video

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   fixturesFolder: false,
   viewportHeight: 300,
   viewportWidth: 300,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // https://github.com/bahmutov/cypress-high-resolution
