450fd19b33cab68bb354ae74eb192852d06dd0b7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-10-28 09:03:24 -0400

    solved

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,20 +1,35 @@
+// intellisense for Cypress commands
 /// <reference types="cypress" />
+// intellisense for cy.getByLabel command
+/// <reference types="cypress-get-by-label" />
 
-it('finds the input elements by their labels', () => {
-  // path with respect to the root folder
+// import the cypress-get-by-label commands
+import 'cypress-get-by-label/commands'
+// import the "x2js" NPM module
+import X2JS from 'x2js'
+
+it('finds the input elements by their labels using XML list', () => {
   cy.visit('cypress/index.html')
-
-  // find the element with the label "First name:"
-  // and type "Joe"
-  // Hint: use https://github.com/bahmutov/cypress-get-by-label
-  //
-  // find the element with the label "Last name:"
-  // and type "Smith"
-  //
-  // check the form inputs using jQuery method
-  // https://api.jquery.com/serializeArray/
-  // which returns an array of "name/value" objects
-  // https://on.cypress.io/get
-  // https://on.cypress.io/invoke
-  // https://glebbahmutov.com/cypress-examples/commands/assertions.html
+  // load the list of labels from the "cypress/fixtures/labels.xml"
+  // and parse it using "x2js" module
+  cy.fixture('labels.xml')
+    .then((xml) => {
+      const x2js = new X2JS()
+      const json = x2js.xml2js(xml)
+      return json.labels.label
+    })
+    // you should get an array of objects
+    .should('be.an', 'array')
+    // Tip: print the list into the DevTools console
+    // using console.table method
+    .then(console.table)
+    // iterate over each label and confirm
+    // the page has a label with that text
+    // and the corresponding input field
+    // has the expected id as listed in "labels.xml"
+    .then((labels) => {
+      labels.forEach((label) => {
+        cy.getByLabel(label.text).should('have.prop', 'id', label.id)
+      })
+    })
 })
