a56aa64115bda7bbcfdfd0bfb05f301cd04c6036
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-04-07 14:46:04 -0400

    lesson a10

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,25 @@ module.exports = defineConfig({
   viewportHeight: 300,
   viewportWidth: 300,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on('task', {
+        printObject(o) {
+          console.log('received object', o)
+
+          // send an object with a circular reference
+          // back to the spec browser process
+          const report = {
+            entry: {
+              passed: true,
+            },
+          }
+          // set a circular reference
+          report.entry.report = report
+
+          return report
+        },
+      })
+    },
     supportFile: false,
   },
 })
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,17 +4,12 @@ it('finds the input elements by their labels', () => {
   // path with respect to the root folder
   cy.visit('cypress/index.html')
 
-  // find the element with the label "First name:"
-  // and type "Joe"
-  // Hint: use https://github.com/bahmutov/cypress-get-by-label
-  //
-  // find the element with the label "Last name:"
-  // and type "Smith"
-  //
-  // check the form inputs using jQuery method
-  // https://api.jquery.com/serializeArray/
-  // which returns an array of "name/value" objects
-  // https://on.cypress.io/get
-  // https://on.cypress.io/invoke
-  // https://glebbahmutov.com/cypress-examples/commands/assertions.html
+  const team = {
+    person: {
+      name: 'Joe',
+    },
+  }
+  // set a circular reference
+  team.person.team = team
+  cy.task('printObject', team)
 })
