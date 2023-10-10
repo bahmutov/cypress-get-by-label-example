163353b78a9ea9c8ab2d91b0591b9154f5e436d0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-10-10 14:10:36 -0400

    playig with cy.task

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,14 @@ module.exports = defineConfig({
   viewportHeight: 300,
   viewportWidth: 300,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on('task', {
+        print(s) {
+          console.log(s)
+          return null
+        },
+      })
+    },
     supportFile: false,
   },
 })
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,19 @@
 /// <reference types="cypress" />
 
+Cypress.Commands.overwrite('log', (log, message, ...args) => {
+  // print the to Cypress Command Log
+  // to preserve the existing functionality
+  log(message, ...args)
+  // send the formatted message down to the Node
+  // callback in the cypress.config.js to be printed to the terminal
+  cy.task('print', [message, ...args].join(', '), { log: false })
+})
+
 it('finds the input elements by their labels', () => {
   // path with respect to the root folder
   cy.visit('cypress/index.html')
 
+  cy.log('Hello')
   // find the element with the label "First name:"
   // and type "Joe"
   // Hint: use https://github.com/bahmutov/cypress-get-by-label
